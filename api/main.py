d633181f27414cddaa47b90bcad44cd0a0c70135
Rostyslav Hnatyshyn <rostyslav.hnatyshyn@gmail.com>
2022-04-04 01:38:07 -0700

    Fixed bug with hostnames not being deleted

--- api/main.py
+++ api/main.py
@@ -1,17 +1,19 @@
 from aiocache import Cache
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, APIRouter
 import asyncio
 from typing import Optional
 from pydantic import BaseModel
 
-#aiocache to store data
 app = FastAPI()
 cache = Cache(Cache.MEMORY)
+
+router = APIRouter(prefix="/api")
+
 
 class ClipboardRequest(BaseModel):
     contents: str
     passphrase: Optional[str] = None
-                        
+
     def isPassphraseCorrect(self, passphrase: str = None) -> bool:
         if self.passphrase is None:
             return True
@@ -19,13 +21,16 @@ class ClipboardRequest(BaseModel):
             if self.passphrase == passphrase:
                 return True
             else:
-                raise HTTPException(status_code=403, detail="Invalid authentication.")
+                raise HTTPException(status_code=403,
+                                    detail="Invalid authentication.")
 
-    def set_passphrase(self, new_passphrase: str, passphrase: str = None) -> status:
+    def set_passphrase(self,
+                       new_passphrase: str,
+                       passphrase: str = None) -> status:
         if self.isPassphraseCorrect(passphrase):
             self.passphrase = new_passphrase
             return status.HTTP_200_OK
-            
+
     def get_contents(self, passphrase: str = None) -> str:
         if self.isPassphraseCorrect(passphrase):
             return self.contents
@@ -34,15 +39,18 @@ class ClipboardRequest(BaseModel):
         if self.isPassphraseCorrect(passphrase):
             self.contents = contents
 
+
 class AuthRequest(BaseModel):
     passphrase: Optional[str] = None
-            
+
+
 async def __getClipboard(hostname: str) -> ClipboardRequest:
     clipboard = await cache.get(hostname)
     if clipboard is not None:
         return clipboard
     else:
         raise HTTPException(404, "Clipboard not found.")
+
 
 async def __addHostname(hostname: str):
     hostnames = await cache.get("hostnames")
@@ -55,41 +63,68 @@ async def __addHostname(hostname: str):
         await cache.set("hostnames", hostnames)
 
 
-@app.post('/kill_clipboard/{hostname}')
-async def kill_clipboard(hostname: str, auth: AuthRequest):
-    clipboard = await cache.get(hostname)
-    if clipboard.isPassphraseCorrect(auth.passphrase):
-        deleted = await cache.delete(hostname)
-        return {"detail": "{deleted} clipboard{s} deleted.".format(deleted=deleted,s="s" if deleted > 1 else "")}
+async def __removeHostname(hostname: str):
+    hostnames = await cache.get("hostnames")
+    if hostnames is None:
+        raise HTTPException(404, "Clipboard does not exist")
+    if hostname in hostnames:
+        hostnames.remove(hostname)
+    else:
+        raise HTTPException(404, "Clipboard does not exist")
 
-    
-@app.post('/get_clipboard/{hostname}')
+
+@router.post('/kill_clipboard/{hostname}')
+async def kill_clipboard(hostname: str, auth: AuthRequest):
+    clipboard = await __getClipboard(hostname)
+    if clipboard.isPassphraseCorrect(auth.passphrase):
+        await __removeHostname(hostname)
+        deleted = await cache.delete(hostname)
+        return {
+            "detail":
+            "{deleted} clipboard{s} deleted.".format(
+                deleted=deleted, s="s" if deleted > 1 else "")
+        }
+
+
+@router.post('/get_clipboard/{hostname}')
 async def get_clipboard(hostname: str, auth: AuthRequest):
     clipboard = await __getClipboard(hostname)
     return clipboard.get_contents(auth.passphrase)
 
 
-@app.get('/get_clipboards')
+@router.get('/get_clipboards')
 async def get_clipboards():
     hostnames = await cache.get("hostnames")
-    return hostnames
+    if hostnames is not None:
+        return hostnames
+    else:
+        raise HTTPException(404, "No clipboards found.")
 
 
-@app.post('/set_clipboard/{hostname}')
+@router.post('/set_clipboard/{hostname}')
 async def set_clipboard(hostname: str, clip: ClipboardRequest):
     clipboard = await __getClipboard(hostname)
     clipboard.set_contents(clip.contents, clip.passphrase)
-    return {"detail": "Set contents to {contents}".format(contents=clip.contents)}
+    return {
+        "detail": "Set contents to {contents}".format(contents=clip.contents)
+    }
 
-@app.post('/create_clipboard/{hostname}', status_code=status.HTTP_201_CREATED)
+
+@router.post('/create_clipboard/{hostname}',
+             status_code=status.HTTP_201_CREATED)
 async def create_clipboard(hostname: str, new_clip: ClipboardRequest):
     """
     Creates a clipboard with the given hostname, contents and passphrase.
     """
-    
+
     try:
         await cache.add(hostname, new_clip)
         await __addHostname(hostname)
-        return {"detail": "Created clipboard {hostname}.".format(hostname=hostname)}
+        return {
+            "detail": "Created clipboard {hostname}.".format(hostname=hostname)
+        }
     except ValueError:
         raise HTTPException(403, "Clipboard already exists.")
+
+
+app.include_router(router)
--- api/tests/test_main.py
+++ api/tests/test_main.py
@@ -8,33 +8,33 @@ def test_no_clipboards_exist():
     pass
 
 def test_create_insecure_clipboard():
-    response = client.post("/create_clipboard/test",
+    response = client.post("/api/create_clipboard/test",
                            json={"contents": "supplied contents"})    
     assert response.status_code == 201
 
 
 def test_create_secure_clipboard():
-    response = client.post("/create_clipboard/test_secure",
+    response = client.post("/api/create_clipboard/test_secure",
                            json={"contents": "supplied contents",
                                  "passphrase": "super_secret"})    
     assert response.status_code == 201
 
 
 def test_duplicate_clipboard():
-    response = client.post("/create_clipboard/test",
+    response = client.post("/api/create_clipboard/test",
                            json={"contents": "supplied contents",
                                  "passphrase": "super_secret"})
     assert response.status_code == 403
     
 
 def test_get_real_insecure_clipboard():
-    response = client.post("/get_clipboard/test", json={"passphrase": ""})
+    response = client.post("/api/get_clipboard/test", json={"passphrase": ""})
     assert response.status_code == 200
     assert response.content == b'"supplied contents"'
 
 
 def test_get_real_insecure_clipboard_with_pass():
-    response = client.post("/get_clipboard/test",
+    response = client.post("/api/get_clipboard/test",
                           json={"passphrase": "no need"}
                           )
     assert response.status_code == 200
@@ -42,38 +42,38 @@ def test_get_real_insecure_clipboard_with_pass():
 
     
 def test_get_real_secure_clipboard():
-    response = client.post("/get_clipboard/test_secure",
+    response = client.post("/api/get_clipboard/test_secure",
                           json={"passphrase": "super_secret"}
                           )
     assert response.status_code == 200
     assert response.content == b'"supplied contents"'
 
 def test_fail_get_real_secure_clipboard():
-    response = client.post("/get_clipboard/test_secure",
+    response = client.post("/api/get_clipboard/test_secure",
                           json={"passphrase": "wrong_secret"}
                           )
     assert response.status_code == 403
 
 def test_fail_get_fake_secure_clipboard():
-    response = client.post("/get_clipboard/test_noexist",
+    response = client.post("/api/get_clipboard/test_noexist",
                           json={"passphrase": "wrong_secret"}
                           )
     assert response.status_code == 404
 
 def test_change_insecure_clipboard():
-   response = client.post("/set_clipboard/test",
+   response = client.post("/api/set_clipboard/test",
                           json={"contents": "new_content"}
                           )
    assert response.status_code == 200
 
 def test_get_real_insecure_clipboard_after_change():
-    response = client.post("/get_clipboard/test",
+    response = client.post("/api/get_clipboard/test",
                            json={"passphrase": ""})
     assert response.status_code == 200
     assert response.content == b'"new_content"'
    
 def test_change_secure_clipboard():
-    response = client.post("/set_clipboard/test_secure",
+    response = client.post("/api/set_clipboard/test_secure",
                            json={"contents": "new_content",
                                  "passphrase": "super_secret"}
                            )
@@ -81,14 +81,14 @@ def test_change_secure_clipboard():
 
 
 def test_get_secure_clipboard_after_change():
-    response = client.post("/get_clipboard/test_secure",
+    response = client.post("/api/get_clipboard/test_secure",
                            json={"passphrase": "super_secret"})
     assert response.status_code == 200
     assert response.content == b'"new_content"'
 
 
 def test_fail_set_secure_clipboard():
-    response = client.post("/set_clipboard/test_secure",
+    response = client.post("/api/set_clipboard/test_secure",
                            json={"contents": "new_content",
                                  "passphrase": "wrong_secret"}
                            )
@@ -96,24 +96,24 @@ def test_fail_set_secure_clipboard():
 
 
 def test_kill_insecure_clipboard():
-    response = client.post("/kill_clipboard/test",
+    response = client.post("/api/kill_clipboard/test",
                            json={"passphrase": ""})
     assert response.status_code == 200
 
 
 def test_fail_get_dead_clipboard():
-    response = client.post("/get_clipboard/test",
+    response = client.post("/api/get_clipboard/test",
                           json={"passphrase": ""})
     assert response.status_code == 404
 
 
 def test_fail_kill_secure_clipboard():
-    response = client.post("/kill_clipboard/test_secure",
+    response = client.post("/api/kill_clipboard/test_secure",
                            json={"passphrase": ""})
     assert response.status_code == 403
 
 
 def test_kill_secure_clipboard():
-    response = client.post("/kill_clipboard/test_secure",
+    response = client.post("/api/kill_clipboard/test_secure",
                            json={"passphrase": "super_secret"})
     assert response.status_code == 200
